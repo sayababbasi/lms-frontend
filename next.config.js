dc2613c94cf04e8d720ce336bc368a87b48716c4
sayababbasi <sayababbasi806@gmail.com>
2026-07-17 13:29:56 +0500

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    API_URL: process.env.API_URL || 'http://localhost:8000/api',
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
