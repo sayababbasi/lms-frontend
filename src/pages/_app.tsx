3680f9976dc787beb53a7f1298e90ec147199609
sayababbasi <sayababbasi806@gmail.com>
2026-07-17 17:39:14 +0500

    feat: configure global favicon in App layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,9 +4,16 @@ import { ThemeProvider } from '../contexts/ThemeContext';
 
 import { Toaster } from 'react-hot-toast';
 
+import Head from 'next/head';
+
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <ThemeProvider>
+            <Head>
+                <link rel="icon" href="/favicon.ico" sizes="any" />
+                <link rel="icon" type="image/png" href="/logo.png" />
+                <link rel="apple-touch-icon" href="/logo.png" />
+            </Head>
             <Component {...pageProps} />
             <Toaster
                 position="bottom-right"
